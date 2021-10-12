29ceed519f9b97d2ac6418c59b3241fb68e56bdf
anatawa12 <anatawa12@icloud.com>
2021-10-12 17:15:45 +0900

    add circle version

--- build/logo.mjs
+++ build/logo.mjs
@@ -15,9 +15,38 @@ for (let pathElement of svgElement.querySelectorAll('path')) {
     pathElement.removeAttribute('font-size')
     pathElement.removeAttribute('font-family')
 }
-await writeDst("logo-full.svg", svgToString(svgElement) + '\n');
+const logoFullSvg = svgToString(svgElement);
+makeRounded(svgElement);
+const logoRoundedFullSvg = svgToString(svgElement);
 
-const minified = svgo.optimize(svgToString(svgElement), {
-    path: 'logo-full.svg',
-})
-await writeDst("logo-min.svg", minified.data);
+await writeDst("logo-full.svg", logoFullSvg + '\n');
+await writeDst("logo-circle-full.svg", logoRoundedFullSvg + '\n');
+await writeDst("logo-min.svg", optimize('logo-fill.svg', logoFullSvg));
+await writeDst("logo-circle-min.svg", optimize('logo-circle-fill.svg', logoRoundedFullSvg));
+
+/**
+ * @param svg {SVGSVGElement}
+ */
+function makeRounded(svg) {
+    const document = svg.ownerDocument;
+    const g = document.createElementNS("http://www.w3.org/2000/svg", "g");
+    const children = Array.from(svg.childNodes)
+    for (let child of children) {
+        svg.removeChild(child);
+        g.appendChild(child);
+    }
+    g.setAttribute("transform", "scale(0.9) translate(40, 40)");
+    svg.appendChild(g);
+    //<circle cx="200" cy="200" r="205" fill="none" stroke="black"/>
+    const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    circle.setAttribute('cx', '200');
+    circle.setAttribute('cy', '200');
+    circle.setAttribute('r', '205');
+    circle.setAttribute('fill', 'none');
+    circle.setAttribute('stroke', 'black');
+    svg.appendChild(circle);
+}
+
+function optimize(path, body) {
+    return svgo.optimize(body, {path}).data
+}
